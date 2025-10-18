138e939c6f1526876fd2f82ff1b535f22b1ea2cb
LiamCHayes <liamcharleshayes@gmail.com>
2025-10-18 09:39:57 -0400

    preparing for scc job

--- self_supervision_detection/unet.py
+++ self_supervision_detection/unet.py
@@ -1,18 +1,28 @@
 """Base case for fly detection. Unet trained on synthetic data."""
 
 import torch
-from torch import nn, optim
+from torch import optim
 from torch.utils.data import DataLoader
-import numpy as np
 import segmentation_models_pytorch as smp
 from segmentation_models_pytorch.encoders import get_preprocessing_fn
-import matplotlib.pyplot as plt
+import argparse
 from utilities import SyntheticData
+from visual_test_model import test_on_selected_images
+
+# parse args
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--images", help="path to the directory of images (end with /)")
+parser.add_argument("-l", "--labels", help="path to the directory of labels (end with /)")
+parser.add_argument("-b", "--backgrounds", help="path to the directory of backgrounds (end with /)")
+args = parser.parse_args()
 
 # Hyperparameters
 epochs = 10
 batch_size = 1
-device = 'cuda'
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
 
 # Set up for training
 model = smp.Unet(
@@ -28,18 +38,33 @@ preprocess_input = get_preprocessing_fn("resnet34", pretrained="imagenet")
 loss_fn = smp.losses.DiceLoss(smp.losses.BINARY_MODE)
 optimizer = optim.Adam(model.parameters(), lr= 0.0001)
 
-image_dir = "../data_generation/dataset/train/images/"
-label_dir = "../data_generation/dataset/train/masks/"
-background_dir = "../data_generation/dataset/backgrounds/"
+image_dir = args.images
+label_dir = args.labels
+background_dir = args.backgrounds
 dataset = SyntheticData(image_dir, label_dir, background_dir)
 dataloader = DataLoader(dataset, batch_size, shuffle=True, num_workers=2)
+
+def full_transform(img):
+    """
+    Full transform that the image goes through from PIL Image to model input
+    Used to test on the real data
+    """
+    img.resize((3072, 2048)) # Dimensions are flipped because img is a PIL.Image object
+    img_transform = dataset.img_transform
+    img = img_transform(img).unsqueeze(0)
+    img = img.permute(0, 2, 3, 1)
+    img = preprocess_input(img)
+    img = img.permute(0, 3, 1, 2).float().to(device)
+    return img
 
 # Training loop
 model.train()
 epoch_losses = [0.0 for _ in range(epochs)]
 for epoch in range(epochs):
     running_loss = 0
-    for images, labels in tqdm(dataloader, desc=f"Epoch {epoch}"):
+    print(f"Epoch {epoch}")
+
+    for images, labels in dataloader:
         images = images.permute(0, 2, 3, 1)
         labels = labels.permute(0, 2, 3, 1).to(device)
         processed_images = preprocess_input(images)
@@ -56,8 +81,10 @@ for epoch in range(epochs):
     # Record loss
     running_loss /= len(dataloader)
     epoch_losses[epoch] = running_loss
+    print(f"Loss: {running_loss}\n")
 
-    # Save a forward pass of real data TODO
+    # Save a forward pass of real data
+    test_on_selected_images(model, full_transform, f"unet_{epoch}")
 
 # make dataframe of losses and save
 training_metrics = pl.DataFrame({
---
+++ self_supervision_detection/visual_test_model.py
@@ -0,0 +1,38 @@
+"""Function to visually test on real (out-of-sample) data"""
+
+import torch
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+image_list = [
+        "../real_data/grapes_frames/grapes_0146.jpeg",
+        "../real_data/grapes_frames/grapes_0147.jpeg",
+        "../real_data/grapes_frames/grapes_0150.jpeg",
+        "../real_data/mosquito_lab1_frames/mosquito_lab1_0008.jpeg",
+        "../real_data/fruit_fly_wall_frames/fruit_fly_wall_0152.jpeg",
+        "../real_data/fruit_fly_wall_frames/fruit_fly_wall_0038.jpeg"
+        ]
+
+def test_on_selected_images(model, transform, label):
+    """Forward pass on selected images and save results"""
+    model.eval()
+    for i, image_path in enumerate(image_list):
+        # load images
+        img = Image.open(image_path)
+
+        # forward pass on model
+        model_input = transform(img)
+        out = model(model_input)
+        out = out.detach().squeeze().cpu().numpy()
+
+        # save image and segmentation pair
+        fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+        axs[0].imshow(img)
+        axs[1].imshow(out)
+        axs[0].axis('off')
+        axs[1].axis('off')
+        plt.tight_layout()
+        plt.savefig(f"{label}_img{i}.png")
+
+    model.train()
