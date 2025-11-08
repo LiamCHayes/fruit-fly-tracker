6fd464e3641a0c3f100c932c08c84e8ce9e7e225
LiamCHayes <liamcharleshayes@gmail.com>
2025-11-08 16:57:17 -0500

    exploring filtering and global frequency space

--- fourier_space/find_fly_frequency.py
+++ fourier_space/find_fly_frequency.py
@@ -5,6 +5,7 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 from scipy import ndimage
+from high_pass_filter import high_pass
 
 # Block images
 class BlockCoords:
@@ -57,7 +58,7 @@ def ft_viz(ft):
     return np.log(np.abs(ft) + 1)
 
 if __name__ == "__main__":
-    fly_seq = "cabinet"
+    fly_seq = "grape"
 
     if fly_seq == "grape":
         dir_path = "../real_data/grapes_frames/"
@@ -93,6 +94,12 @@ if __name__ == "__main__":
         ft_shifted = np.fft.fftshift(ft)
         fts.append(ft_shifted)
 
+        # high pass filter frequency space
+        ft_filtered = high_pass(ft_shifted, 10)
+        ft_ifft_shfited = np.fft.ifftshift(ft_filtered)
+        img_filtered = np.fft.ifft2(ft_ifft_shfited)
+        img_filtered = np.abs(img_filtered)
+
         # high pass filter
         kernel = np.array([[-1, -1, -1],
                            [-1, 8, -1],
@@ -104,9 +111,9 @@ if __name__ == "__main__":
         axs[0].imshow(frame)
         axs[1].imshow(block)
         axs[2].imshow(ft_viz(ft_shifted))
-        axs[3].imshow(high_filtered, cmap='gray')
+        axs[3].imshow(ft_viz(ft_filtered))
         plt.tight_layout()
-        plt.savefig(f"cabinet_fly{i}.png")
+        # plt.savefig(f"cabinet_fly{i}.png")
         plt.show()
 
     # compare fruit fly blocks vs non fruit fly blocks in frequency space
---
+++ fourier_space/frequency_video.py
@@ -0,0 +1,54 @@
+"""Animate the frequency space of a moving fruit fly video"""
+
+import os
+import cv2
+from PIL import Image
+import numpy as np
+from tqdm import tqdm
+from high_pass_filter import high_pass
+
+# load grapes frames
+dir_path = "../real_data/grapes_frames/"
+paths = [dir_path + path for path in os.listdir(dir_path)]
+paths.sort()
+
+# video writer
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+video = cv2.VideoWriter("fft_plots/grapes_video_filtered.avi", fourcc, 30, (3072, 1024))
+
+def ft_viz(ft):
+    return np.log(np.abs(ft) + 1)
+
+frames = []
+for path in tqdm(paths):
+    # load image
+    frame = Image.open(path)
+    frame = np.array(frame)
+    gray_frame = np.mean(frame, axis=2)
+
+    # get corresponding global fourier transform
+    ft = np.fft.fft2(gray_frame)
+    ft_shifted = np.fft.fftshift(ft)
+    ft_filtered = high_pass(ft_shifted)
+
+    # make frame
+    frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+    ft_viz_frame = ft_viz(ft_filtered)
+    ft_viz_norm = cv2.normalize(ft_viz_frame, None, 0, 255, cv2.NORM_MINMAX)
+    ft_viz_uint8 = ft_viz_norm.astype(np.uint8)
+    ft_3ch = cv2.cvtColor(ft_viz_uint8, cv2.COLOR_GRAY2BGR)
+
+    full_frame = np.hstack((frame_bgr, ft_3ch))
+
+    # downsample
+    scale_percent = 50  # percent of original size (e.g., 50%)
+    width = int(full_frame.shape[1] * scale_percent / 100)
+    height = int(full_frame.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    downsampled_frame = cv2.resize(full_frame, dim, interpolation=cv2.INTER_AREA)
+
+    # animate
+    video.write(downsampled_frame)
+
+video.release()
